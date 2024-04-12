1160a338b160a418e69d4b83deeb119bc0c09194
ajayniveusian <sharmaajay042000@gmail.com>
2024-04-12 17:08:19 +0530

    Update rough.js

--- rough.js
+++ rough.js
@@ -1,6 +1,18 @@
+// just a rough work space to test logics
+// just a rough work space to test logics
+// just a rough work space to test logics
+// just a rough work space to test logics
+
+
 const filters = (fobj) =>{
     return fobj.map(e=> `${e.name} = ${e.value} AND`)
 }
 
 
 console.log(filters([{name: 'a', value: 1}, {name: 'b', value: 2}]).join(" "))
+
+
+// just a rough work space to test logics
+// just a rough work space to test logics
+// just a rough work space to test logics
+// just a rough work space to test logics
